6d898f42835df5498aa6282dcf0dfbece04c47f0
Lourenço Ponces <lourenco.poncesduarte@gmail.com>
2020-11-23 23:09:56 +0000

    Remove source path

--- taint.py
+++ taint.py
@@ -1,12 +1,7 @@
 class Taint:
-    def __init__(self, value = False, initial_sources = None, sources_path = (), sanitizers = (), sinks = ()):
+    def __init__(self, value = False, initial_sources = None, sanitizers = (), sinks = ()):
         self.value = value
         self.initial_sources = initial_sources
-
-        tmp1 = f(source1, source2)
-        tmp2 = g(tmp1, source3)
-        # ((initial_sourceA -> sourceA2 -> ... -> most_recent_sourceA), (initial_sourceA -> sourceA2 -> ... -> most_recent_sourceA))        
-        self.sources_path = sources_path
         self.sanitizers = sanitizers
         self.sinks = sinks
     
@@ -15,3 +10,9 @@ class Taint:
     
     def get_initial_sources(self):
         return self.initial_sources
+    
+    def get_sanitizers(self):
+        return self.sanitizers
+
+    def get_sinks(self):
+        return self.sinks
