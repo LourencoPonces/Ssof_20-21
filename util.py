593d9dae40768eeb618e718cbfdca5fe732d8ae3
afonsocrg <afonso.crg@gmail.com>
2020-11-26 15:41:26 +0000

    add debug funcion

--- util.py
+++ util.py
@@ -1,5 +1,12 @@
 import sys
 
+debugging = True
+
 def fatal(msg):
     print(f"[Error] {msg}")
     sys.exit(-1)
+
+
+def debug(msg, level = 0):
+    if debugging:
+        print(f'[DBG] {"  "*level}{msg}')
