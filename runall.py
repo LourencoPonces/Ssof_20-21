3c735052d5a37effc6eb80cba817b58e33ee659f
afonsocrg <afonso.crg@gmail.com>
2020-12-03 16:29:38 +0000

    try tests

--- runall.py
+++ runall.py
@@ -6,16 +6,15 @@ from util import *
 from read_files import read_json
 
 
-PASSED = 0
-FAILED = 1
-NO_OUT = 2
-NO_EXP = 3
+def report_result(test_name, result):
+    cols = os.get_terminal_size(1).columns
+    test_name = test_dir.stem
+    num_dash = cols - len(result) - len(test_name) - 7
+    print(f'[ {test_name} {"-"*num_dash}> {color_result(result)} ]')
 
-
-
-def verify_output(program_path):
-    out_path = get_out_filepath(program_path)
-    exp_path = get_expected_filepath(program_path)
+def verify_output(test_dir):
+    out_path = get_out_filepath(test_dir)
+    exp_path = get_exp_filepath(test_dir)
 
     if not out_path.exists():
         return NO_OUT
@@ -28,7 +27,8 @@ def verify_output(program_path):
     return PASSED if sort_dict(out) == sort_dict(exp) else FAILED
 
 if __name__ == '__main__':
-    debugging = False
+    init(debug = False, color = True)
+
     if len(sys.argv) != 1 + 2:
         fatal(f'Usage: {sys.argv[0]} <program_directory> <patterns.json>')
 
@@ -45,7 +45,16 @@ if __name__ == '__main__':
     slices.sort()
     for slice_path in slices:
 
-        go(slice_path, pattern_path)
+        if out_path.exists():
+            out_path.unlink()
+
+        try:
+            go(ast_path, pattern_path)
+            result = verify_output(test_dir)
+        except:
+            result = ERROR
+
+        report_result(test_dir, result)
 
         result = verify_output(slice_path)
         if result == PASSED:
